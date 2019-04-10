579caa6755248018b0debc97d3c72dabd25e79fd
theMistletoe <katachiwotodomete@gmail.com>
2019-04-11 07:59:34 +0900

    write contract code ERC721

---
+++ contracts/BookReportToken.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+
+import "./../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+
+contract BookReportToken is ERC721Full {
+    uint256 internal nextTokenId = 0;
+
+    constructor(string memory  name, string memory symbol) ERC721Full(name, symbol) public {}
+
+    event Mint(address owner, uint256 tokenId);
+
+    function createBookReport(string memory tokenURI) public returns (uint256) {
+
+        uint256 tokenId = nextTokenId;
+        nextTokenId = nextTokenId.add(1);
+
+        super._mint(msg.sender, tokenId);
+        super._setTokenURI(tokenId, tokenURI);
+
+        emit Mint(msg.sender, tokenId);
+
+        return tokenId;
+    }
+}

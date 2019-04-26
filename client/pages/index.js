f989f7a925cc549700bf1b1aa28f9bcd7c10ba1e
theMistletoe <katachiwotodomete@gmail.com>
2019-04-26 18:40:06 +0900

    update: move content to mypage

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,9 @@
-import Layout from '../comps/MyLayout';
-import NetworkInfo from '../comps/NetworkInfo'
-import AccountInfo from '../comps/AccountInfo';
-import AllReports from '../comps/AllReports';
-import PostReportForm from '../comps/PostReportForm'
+import Layout from '../comps/MyLayout'
+import AllReports from '../comps/AllReports'
 
 const Index = (props) => (
     <Layout>
         <h1>Book Reports</h1>
-        <NetworkInfo />
-        <AccountInfo />
-        <PostReportForm />
         <AllReports />
     </Layout>
 )
--- client/pages/mypage.js
+++ client/pages/mypage.js
@@ -1,7 +1,13 @@
-import Layout from "../comps/MyLayout";
+import Layout from "../comps/MyLayout"
+import NetworkInfo from '../comps/NetworkInfo'
+import AccountInfo from '../comps/AccountInfo'
+import PostReportForm from '../comps/PostReportForm'
 
 export default () => (
     <Layout>
         <p>This is the your account page</p>
+        <NetworkInfo />
+        <PostReportForm />
+        <AccountInfo />
     </Layout>
 )

9d6a6b7df93f7e2326a2b4bebef54a4cd9589d15
theMistletoe <katachiwotodomete@gmail.com>
2019-05-17 08:31:25 +0900

    make top

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,26 @@
 import Layout from "../comps/MyLayout";
+import { emphasize } from "material-ui/utils/colorManipulator";
 
 export default () => (
   <Layout>
-    <p>This is the about page</p>
+    <center>
+      <h1>IRead</h1>
+      <p>自分の読んだ本の感想文をEthereumチェーンで管理できる分散アプリケーション</p>
+    </center>
+    <img src="/static/bookcase.jpg" />
+
+    <h2>Usage</h2>
+
+    <emphasize>※注意</emphasize>
+    <p>アプリケーションにEthereumのRopstenTestNetworkを利用します。</p>
+    <p>Metamaskをインストールし、Ether FaucetからRopstenTestNetworkのETHを入手してからご利用ください。</p>
+    <ul>
+      <li>
+        <a href='https://metamask.io/' >Metamask</a>
+      </li>
+      <li>
+        <a href='https://faucet.metamask.io/'>Ether Faucet</a>
+      </li>
+    </ul>
   </Layout>
 )

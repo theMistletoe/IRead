5f1ad9d878fe798e769ff88ee191e171533f0a90
theMistletoe <katachiwotodomete@gmail.com>
2019-05-17 20:06:34 +0900

    update: add footer

--- client/comps/MyLayout.js
+++ client/comps/MyLayout.js
@@ -1,10 +1,13 @@
 import Header from './Header';
 import Head from 'next/head';
+import { darkBlack } from 'material-ui/styles/colors';
+import { white } from 'material-ui/styles/colors';
 
-const layoutStyle = {
-    margin: 20,
-    padding: 20,
-    border: '1px solid #DDD'
+
+const footerStyle = {
+    backgroundColor: darkBlack,
+    marginRight: 15,
+    color: white
 }
 
 const Layout = (props) => (
@@ -15,6 +18,9 @@ const Layout = (props) => (
         </Head>
         <Header />
         {props.children}
+        <footer style={footerStyle}>
+            @2019 <a href='https://github.com/theMistletoe'>theMistletoe</a> All Right Reserved.
+        </footer>
     </div>
 )
 
